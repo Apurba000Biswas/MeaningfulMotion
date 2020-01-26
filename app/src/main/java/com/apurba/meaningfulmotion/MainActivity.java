9f2f1439d107919061b980720ee6b8db83749d76
apurba <apurba10024@gmail.com>
2020-01-27 01:06:01 +0600

    1.1 Added Image Transition on Button Click

--- app/src/main/java/com/apurba/meaningfulmotion/MainActivity.java
+++ app/src/main/java/com/apurba/meaningfulmotion/MainActivity.java
@@ -3,6 +3,12 @@ package com.apurba.meaningfulmotion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.transition.Slide;
+import android.transition.TransitionManager;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onHideButtonClick(View view) {
+        Slide slide = new Slide();
+        slide.setSlideEdge(Gravity.TOP);
+
+        ViewGroup viewGroup = findViewById(R.id.main_container);
+        TransitionManager.beginDelayedTransition(viewGroup, slide);
+        ImageView image = findViewById(R.id.imageView);
+        image.setVisibility(View.GONE);
     }
 }
